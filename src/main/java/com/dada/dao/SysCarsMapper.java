d8b1e76dac22e7a1a11e0ae4ff96d1db25423c2b
Administrator <Administrator@SKY-20170327XQQ>
2019-04-27 01:10:00 +0800

    汽车模块第一次

--- src/main/java/com/dada/dao/SysCarsMapper.java
+++ src/main/java/com/dada/dao/SysCarsMapper.java
@@ -1,12 +1,21 @@
 package com.dada.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.dada.entity.SysCars;
 
 @Mapper
 public interface SysCarsMapper {
-    int insert(SysCars record);
+	boolean insert(SysCars record);
 
-    int insertSelective(SysCars record);
+	boolean insertSelective(SysCars record);
+    
+    
+    List<Map<String, Object>> selectAllInfo(SysCars record);
+    
+    boolean updateInfo(SysCars record);
+    
 }
